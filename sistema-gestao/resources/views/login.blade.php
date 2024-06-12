cc536c460ceb647840582effe7b1b50ef1eda795
IgorOliverx <igorferoli019@gmail.com>
2024-06-12 10:58:12 -0300

    commit

--- sistema-gestao/resources/views/login.blade.php
+++ sistema-gestao/resources/views/login.blade.php
@@ -8,7 +8,7 @@
     ">
         <nav class="navbar navbar-expand-lg pl-5 pr-5">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">
+                <a class="navbar-brand" href="/">
                     <img src="images/Logo-senai-bw.png" alt="Logo-Senai preto e branco" width="150px" height="40px" id="img-logo">
                     <a href="" class="navbar-brand ml-0" style="position: relative; bottom: -8px" >
                         <p class="text-primary text-light">
