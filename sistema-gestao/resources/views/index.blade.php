b80b7b39c56b78db3a094288ba20505e8b1b68bc
Ezequielzz <ezequielsamueldl@gmail.com>
2024-09-05 16:19:34 -0300

    Reformulando Home

--- sistema-gestao/resources/views/index.blade.php
+++ sistema-gestao/resources/views/index.blade.php
@@ -26,9 +26,42 @@
             gap: 15px;
         }
 
+        .btn-container #btn2 {
+            width: 145px;
+            height: 55px;
+            background-color: white;
+            border-radius: 7px;
+            transition: 0.35s
+        }
+
+        .btn-container #btn2:hover {
+            color: black;
+            background-color: rgb(239, 239, 239);
+        }
+
+        .btn-container #btn1 {
+            width: 145px;
+            height: 55px;
+            background-color: rgb(0, 0, 0);
+            border-radius: 7px;
+            transition: 0.35s
+        }
+
+        .btn-container #btn:hover {
+            color: rgb(255, 255, 255);
+            background-color: rgb(39, 39, 39);
+        }
+
+        .btn-container a {
+            font-family: "Montserrat", sans-serif;
+            color: black;
+            text-decoration: none;
+            font-weight: 700;
+        }
+
         .custom-header img {
-            width: 500px;
-            height: 350px;
+            width: 600px;
+            height: 450px;
         }
 
         .infos {
@@ -39,12 +72,12 @@
         }
 
         .infos .infosTitulo h3 {
-            overflow-y: hidden;
             display: flex;
             text-align: center;
             font-size: 35px;
-            font-family: "Poppins", sans-serif;
-            font-weight: 700;
+            font-family: "Montserrat", sans-serif;
+            font-optical-sizing: auto;
+            font-weight: 600;
             font-style: normal;
         }
 
@@ -69,24 +102,54 @@
 
         .container {
             display: flex;
-            justify-content: space-between;
+            justify-content: space-evenly;
             margin-top: 70px;
         }
 
         .imgContainer {
-            width: 500px;
-            margin-right: 30px;
+            width: 350px;
         }
 
         .textContainer {
             width: 800px;
             display: flex;
             flex-direction: column;
+            align-items: flex-start;
+            font-size: 17px;
+            font-family: "Montserrat", sans-serif;
         }
 
         .title {
+            font-family: "Montserrat", sans-serif;
+            font-optical-sizing: auto;
+            font-weight: 500;
+            font-style: normal;
             font-size: 40px;
-            font-weight: 700;
+        }
+
+        .containerInvertido {
+            display: flex;
+            justify-content: space-evenly;
+            margin-top: 70px;
+        }
+
+        .textContainerInvertido {
+            width: 800px;
+            display: flex;
+            flex-direction: column;
+            align-items: flex-end;
+            font-size: 17px;
+            font-family: "Montserrat", sans-serif;
+        }
+
+        .titleInvertido {
+            display: flex;
+            align-self: flex-start;
+            font-family: "Montserrat", sans-serif;
+            font-optical-sizing: auto;
+            font-weight: 500;
+            font-style: normal;
+            font-size: 40px;
         }
 
         .btn button {
@@ -102,23 +165,28 @@
 
     <div class="custom-header">
         <div>
-            <h2>Bem-vindo ao sistema de <br> gerenciamento de estoque e <br> ativos patrimoniais do SENAI 505</h2>
+            <h2>Bem-vindo ao sistema de gerenciamento de estoque e <br> ativos patrimoniais do SENAI 505</h2>
             <div class="btn-container">
-                <a href="{{ route('login') }}">
-                    Login
+                <a href="{{ route('login') }}" style="color: white">
+                    <button id="btn1">
+                        Login
+                    </button>
                 </a>
+
                 <a href="">
-                    Saber Mais
+                    <button id="btn2">
+                        Saber Mais
+                    </button>
                 </a>
             </div>
         </div>
         <div>
-            <img src="/images/welcome.jpg" alt="Imagem de boas-vindas">
+            <img src="/images/Welcome-cuate.svg" alt="Imagem de boas-vindas">
         </div>
     </div>
     <div class="infos">
         <div class="infosTitulo">
-            <h3>Gerencie os ativos patrimoniais <br> e estoque em único sistema</h3>
+            <h3>Gerencie os ativos patrimoniais <br> e estoque em um único sistema</h3>
         </div>
         <div class="infosIcon">
             <div class="teste">
@@ -137,21 +205,49 @@
     </div>
     <div class="container">
         <div class="imgContainer">
-            <img src="/images/Company-amico.svg" alt="">
+            <img src="/images/Checking boxes-rafiki.svg" alt="">
         </div>
         <div class="textContainer">
-            <div class="title">Lorem ipsum dolor sit amet</div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi fugiat modi itaque voluptatem non quidem possimus
-            reprehenderit, eius repellendus eveniet sequi natus eaque minus ipsa velit, porro totam. Dignissimos, qui.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi fugiat modi itaque voluptatem non quidem possimus
-            reprehenderit, eius repellendus eveniet sequi natus eaque minus ipsa velit, porro totam. Dignissimos, qui.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi fugiat modi itaque voluptatem non quidem possimus
-            reprehenderit, eius repellendus eveniet sequi natus eaque minus ipsa velit, porro totam. Dignissimos, qui.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi fugiat modi itaque voluptatem non quidem possimus
-            reprehenderit, eius repellendus eveniet sequi natus eaque minus ipsa velit, porro totam. Dignissimos, qui.
-            <div class="btn">
-                <button>Teste</button>
-            </div>
+            <div class="title">Conheça o Sistema SENAI 505</div>
+            Nosso sistema de gerenciamento de estoque e ativos patrimoniais foi desenvolvido para otimizar o controle e a
+            organização dos recursos da unidade SENAI 505. Com uma interface simples e eficiente, você pode acompanhar o uso
+            dos ativos em tempo real, controlar o estoque com precisão e garantir o funcionamento contínuo dos equipamentos
+            essenciais para as operações. Além disso, o sistema permite a emissão de relatórios detalhados para análise e
+            planejamento, ajudando a tomar decisões baseadas em dados concretos.
+
         </div>
     </div>
+    <div class="containerInvertido">
+        <div class="textContainerInvertido">
+            <div class="titleInvertido">Controle Total de Ativos e Estoque</div>
+            Reduza o desperdício e aumente a eficiência operacional com uma gestão integrada de ativos e estoque. O sistema
+            permite que você registre a entrada e saída de itens, monitore o uso de equipamentos e rastreie ativos desde sua
+            aquisição até o fim de sua vida útil. Essa rastreabilidade completa proporciona transparência e segurança,
+            garantindo que os recursos estejam sempre disponíveis quando necessários e que os processos de manutenção e
+            reposição sejam realizados no momento certo.
+            Além disso, a gestão patrimonial do SENAI 505 oferece alertas automáticos para controle de prazos e manutenções
+            preventivas, ajudando a evitar perdas e garantindo a longevidade dos equipamentos.
+
+        </div>
+        <div class="imgContainer">
+            <img src="/images/Team spirit-pana.svg" alt="">
+        </div>
+    </div>
+    <div class="container">
+        <div class="imgContainer">
+            <img src="/images/Company-rafiki.svg" alt="">
+        </div>
+        <div class="textContainer">
+            <div class="title">Colaboração Eficiente Entre Equipes</div>
+            Nosso sistema é ideal para facilitar a comunicação e a colaboração entre equipes de diferentes setores. Com
+            todos os dados centralizados e acessíveis em um único lugar, todos os membros da unidade podem acessar as
+            informações necessárias em tempo real. Isso garante que a gestão dos recursos seja feita de maneira mais
+            eficiente e organizada.
+            Com a automação de tarefas repetitivas e a padronização de processos, você otimiza o fluxo de trabalho e libera
+            sua equipe para focar em atividades mais estratégicas. A integração com outros sistemas internos também permite
+            uma visão holística das operações, garantindo maior controle e planejamento.
+
+        </div>
+    </div>
+    <x-footer></x-footer>
 @endsection
--- sistema-gestao/resources/views/layouts/master.blade.php
+++ sistema-gestao/resources/views/layouts/master.blade.php
@@ -3,14 +3,16 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     @notifyCss
     <link rel="icon" href="/images/favicon.png" type="image/x-icon">
 
     <!-- Link master Bootstrap -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
     <!-- Links de importação do bootstrap -->
     <link rel="stylesheet" href="/css/style.css">
@@ -20,16 +22,20 @@
 
 
     <!-- Título da Página  -->
-    <title>{{config('master.name', 'SENAI - Gestão')}}</title>
+    <title>{{ config('master.name', 'SENAI - Gestão') }}</title>
 
     <!-- Fontes devem ser importadas abaixo -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+        rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Abel&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Abel&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+        rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap" rel="stylesheet">
@@ -38,7 +44,13 @@
     <link href="https://fonts.googleapis.com/css2?family=Teko:wght@500&display=swap" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
 
 
     <!-- Links de lib externas JS -->
@@ -72,9 +84,15 @@
 
     <!-- JavaScript Bootstrap -->
     <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
+    </script>
     @notifyJs
 
 </body>
